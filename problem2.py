1284d1574ecf022e504c940ac142e0749f09bd51
81946 <71406891+81946@users.noreply.github.com>
2020-10-05 11:08:38 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -23,3 +23,35 @@ Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
+num= input("Enter a number")
+num= float(num)
+
+num2= input("Enter another number")
+num2= float(num2)
+
+
+
+if num > num2:
+    if num % num2==0:
+        num= str(num)
+        num2= str(num2)
+        print(num2 + ' ' + "is a factor of" + ' ' + num)
+
+elif num2 % num==0:
+    num= str(num)
+    num2= str(num2)
+    print(num + ' '+ "is a factor of" + ' '+ num2)
+
+if num > num2:
+    if num % num2!=0:
+        num= str(num)
+        num2= str(num2)
+        print(num2 + ' ' + "is not a factor of" + ' ' + num)
+
+elif num % num2!=0:
+    num= str(num)
+    num2= str(num2)
+    print(num + ' ' + "is not a factor of" + ' ' + num2)
+
+
+
