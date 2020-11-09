048e4b6e11ff0f04151fbc0a1ffcb96800d377f4
81946 <71406891+81946@users.noreply.github.com>
2020-11-09 14:22:36 -0800

    update

--- problem3.py
+++ problem3.py
@@ -26,4 +26,62 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+import math
+
+x= input("Enter a number:")
+y= input("Enter a number:")
+z= input("Enter a number:")
+x= int(x)
+y= int(y)
+z= int(z)
+
+
+if x < y and z < y:
+    answer= pow(x,2)+pow(z,2)
+    answer2= math.sqrt(answer)
+    if answer2==y:
+        x= str(x)
+        y= str(y)
+        z= str(z)
+        print(x + "," + y + "," + z + ' ' + "form a Pythagorean triple")
+    else:
+        x= str(x)
+        y= str(y)
+        z= str(z)
+        print(x + "," + y + "," + z + ' ' +  "do not form a Pythagorean triple")        
+
+if y < z and x < z:
+    answer= pow(y,2)+pow(x,2)
+    answer2= math.sqrt(answer)
+    if answer2==z:
+        x= str(x)
+        y= str(y)
+        z= str(z)
+        print(y + "," + x + "," + z + ' ' + "form a Pythagorean triple")
+    else:
+        x= str(x)
+        y= str(y)
+        z= str(z)
+        print(y + "," + x + "," + z + ' ' +  "do not form a Pythagorean triple")
+
+
+if z < x and y < x:
+    answer= pow(z,2)+pow(y,2)
+    answer2= math.sqrt(answer)
+    if answer2==x:
+        x= str(x)
+        y= str(y)
+        z= str(z)
+        print(z + "," + y + "," + x + ' ' + "form a Pythagorean triple")
+    else:
+        x= str(x)
+        y= str(y)
+        z= str(z)
+        print(z + "," + y + "," + x + ' ' +  "do not form a Pythagorean triple")
+
+
+
+
+
+
 
